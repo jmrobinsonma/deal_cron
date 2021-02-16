d9e9aa0d86794830c7dd464ae5c726d57d37a6d9
jmrobinson <jmrobinsonma@gmail.com>
2021-02-16 16:50:43 -0500

    minor

--- make_file.py
+++ make_file.py
@@ -1,3 +1,4 @@
+import os
 # Get the user input
 
 url_list = []
@@ -13,28 +14,30 @@ while next_url != 'a':
 	next_url = input("\nEnter another url or 'a' to advance: ")
 
 # Format the urls
-
-flagged_urls = ""
+formatted_urls = ""
 
 for i in url_list:
-	flagged_urls = flagged_urls + f" -u '{i}'"
+	formatted_urls = formatted_urls + f" -u '{i}'"
 
 # Format the shell script
-
 shell = "#!bin/bash"
-
 python_path = "/home/jmr/scrapers/deal_cron/venv/bin/python3"
 dot_py_path = "/home/jmr/scrapers/deal_cron/dealcron.py"
-shell_script_path = f"/home/jmr/scrapers/deal_cron/{user_search_name}.sh"
+project_path = f"/home/jmr/scrapers/deal_cron/{user_search_name}"
+shell_script_path = f"{project_path}/{user_search_name}.sh"
 
-shell_script = f"{shell}\n{python_path} {dot_py_path} {user_search_name} {user_email}{flagged_urls}"
+os.system(f"mkdir {project_path}")
 
+shell_script = f"{shell}\n{python_path} {dot_py_path} {user_search_name} {user_email}{formatted_urls}"
 with open(shell_script_path, "w") as f:
 	f.write(shell_script)
 
+os.system(f". {shell_script_path}")
+
 # Format a docker run command and shell script
+#docker_run_command = f"docker run --name {user_search_name} -idt jmr/dealtest /bin/bash -c 'python3 dealcron.py {user_search_name} {user_email}{formatted_urls}'"
+#os.system(docker_run_command)
 
-#docker_run = f"docker run --name {user_search_name} -idt jmr/jobcron /bin/bash -c 'python3 dealcron.py {user_search_name} {user_email}{flagged_urls}'"
-#docker_script = f"{shell}\n{docker_run}"
-
-
+#docker_start_script = f"{shell}\ndocker start {user_search_name}"
+#with open(shell_script_path, "w") as f:
+#	f.write(docker_start_script)
